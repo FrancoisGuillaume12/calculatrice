34f5a26d59b013851fcffda3daca961396fcc5d8
FrancoisGuillaume12 <76703672+FrancoisGuillaume12@users.noreply.github.com>
2021-11-02 14:54:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let choix;
 let nombreA;
 let nombreB;
 let total;
+let restart ; 
 
 function addition(nombre1, nombre2 ) {
    
@@ -30,52 +31,62 @@ function division(nombre1, nombre2) {
         return nombre1 / nombre2;
     }   
 }
-   
-
-do{
-   
-    choix = Number(prompt("entrez vortre choix \n \n 1: addition \n 2: mutiplication \n 3: soustartion \n 4: division"));
-
-}while(choix != 1  && choix != 2  && choix != 3  && choix != 4)
 
 
+do {  
 
-do{
-
-    nombreA = Number(prompt("entrez un premier chiffre a calcullez"))
-    nombreB = Number(prompt("entrez un deuxieme chiffre a calcullez"))
-
-}while (isNaN(nombreA) || isNaN(nombreB)) 
+    do{
     
+        choix = Number(prompt("entrez vortre choix \n \n  1: addition \n 2: soustartion   \n 3: mutiplication  \n 4: division"));
 
-
-
-try{
-    switch (choix) {
-        case 1:
-            total = addition(nombreA, nombreB);
-            break;
+    }while(choix != 1  && choix != 2  && choix != 3  && choix != 4){
         
-        case 2:
-            total = soustration(nombreA, nombreB);
-            break;
-
-        case 3:
-            total = multiplication(nombreA, nombreB);
-            break;
-        
-        case 4:
-            total = division(nombreA, nombreB);
-            break;
-        
-        default:
-            throw new Error("tu c'est pas lire ou quoi ?");
-
     }
 
-    alert(" résultat : " + total);
-}
 
-catch(ereur){
-    allert("ereur");
+
+    do{
+
+        nombreA = Number(prompt("entrez un premier chiffre a calcullez"))
+        nombreB = Number(prompt("entrez un deuxieme chiffre a calcullez"))
+
+    }while (isNaN(nombreA) || isNaN(nombreB)) 
+        
+
+
+
+    try{
+        switch (choix) {
+            case 1:
+                total = addition(nombreA, nombreB);
+                break;
+            
+            case 2:
+                total = soustration(nombreA, nombreB);
+                break;
+
+            case 3:
+                total = multiplication(nombreA, nombreB);
+                break;
+            
+            case 4:
+                total = division(nombreA, nombreB);
+                break;
+            
+            default:
+                throw new Error("tu c'est pas lire ou quoi ?");
+
+        }
+
+        alert(" résultat : " + total);
+    }
+
+    catch(ereur){
+        alert("ereur");
+    }
+
+    restart = confirm("Souhaitez-vous recommencer un calcul ?");
+  
+} while(restart){
+
 }
